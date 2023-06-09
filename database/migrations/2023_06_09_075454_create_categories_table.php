fca161fbc745bd59f2fb588aaf095ff42cf53c00
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 09:56:13 +0200

    Define the migration to add the categories table

--- database/migrations/2023_06_09_075454_create_categories_table.php
+++ database/migrations/2023_06_09_075454_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 150)->unique();
+            $table->string('slug');
             $table->timestamps();
         });
     }
