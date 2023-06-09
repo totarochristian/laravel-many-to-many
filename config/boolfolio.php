542e1eddc17f2453b1ecbf2c1d6b424152dfc06a
totarochristian <totaro.christian.18@gmail.com>
2023-06-09 10:24:26 +0200

    Define ProjectSeeder and add file boolfolio with the data used to seed

---
+++ config/boolfolio.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'projects' => [
+        [
+            "image" => "",
+            "title" => "Spotify web",
+            "body" => "This is a solution to the html-css-spotifyweb challenge of Boolean. Boolean help you improve your coding skills by building realistic projects with an increasingly high level of difficulty.",
+            "live_site" => "https://html-css-spotifyweb-lybb0cm33-totarochristian.vercel.app",
+            "git_repository" => "https://github.com/totarochristian/html-css-spotifyweb"
+        ],
+        [
+            "image" => "",
+            "title" => "Boolzapp",
+            "body" => "This is a solution to the vue-boolzap challenge of Boolean. Boolean help you improve your coding skills by building realistic projects with an increasingly high level of difficulty.",
+            "live_site" => "",
+            "git_repository" => "https://github.com/totarochristian/vue-boolzapp"
+        ],
+        [
+            "image" => "",
+            "title" => "Boolflix",
+            "body" => "This is a solution to the vite-boolflix challenge of Boolean. Boolean help you improve your coding skills by building realistic projects with an increasingly high level of difficulty.",
+            "live_site" => "https://vite-boolflix-snowy.vercel.app/",
+            "git_repository" => "https://github.com/totarochristian/vite-boolflix"
+        ]
+    ]
+];
