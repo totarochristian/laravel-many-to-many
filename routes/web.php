bd64cad95275d86ff212b592005974a2cf3b8751
totarochristian <totaro.christian.18@gmail.com>
2023-06-11 23:07:39 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
 
