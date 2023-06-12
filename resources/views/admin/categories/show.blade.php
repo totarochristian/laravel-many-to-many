1bbd9123e29b58c4bbeb1720fdc02750e15fe213
totarochristian <totaro.christian.18@gmail.com>
2023-06-12 09:54:07 +0200

    Update show page of categories

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.admin')
 @section('content')
-    <h1>Category</h1>
-    <p>The category name is: {{ $category->name }}</p>
-    @foreach ($category->projects as $proj)
-        <p>{{ $proj->title }}</p>
-    @endforeach
+    <h1>{{ $category->name }}</h1>
+    <p>
+        <h4>Progetti appartenenti a questa categoria:</h4>
+        <ul class="fs-5">
+            @foreach ($category->projects as $proj)
+                <li><a href="{{ route('admin.projects.show', $proj->slug) }}">{{ $proj->title }}</a></li>
+            @endforeach
+        </ul>
+    </p>
 @endsection
