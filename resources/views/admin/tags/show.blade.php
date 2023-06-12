436a049e44767d752cf94473a842a0aec79c1bd5
totarochristian <totaro.christian.18@gmail.com>
2023-06-12 09:55:15 +0200

    Update show page of tags

--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.admin')
 @section('content')
-    <h1>Tag</h1>
-    <p>The tag name is: {{ $tag->name }}</p>
-    @foreach ($tag->projects as $proj)
-        <p>{{ $proj->title }}</p>
-    @endforeach
+    <h1>{{ $tag->name }}</h1>
+    <p>
+        <h4>Progetti che utilizzano questa tecnologia:</h4>
+        <ul class="fs-5">
+            @foreach ($tag->projects as $proj)
+                <li><a href="{{ route('admin.projects.show', $proj->slug) }}">{{ $proj->title }}</a></li>
+            @endforeach
+        </ul>
+    </p>
 @endsection
