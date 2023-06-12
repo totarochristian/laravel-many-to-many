dd2f38c47bc1d6c621b0b1dd8a5d1470ede70192
totarochristian <totaro.christian.18@gmail.com>
2023-06-12 09:26:50 +0200

    Update categories and project models + routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
