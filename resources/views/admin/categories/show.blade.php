a46bb0ba029c43b62ded9d49d6ebb4cba6867a82
totarochristian <totaro.christian.18@gmail.com>
2023-06-12 11:18:20 +0200

    Update show pages for tags and categories to manage the case where there aren't projects related to this objects

--- resources/views/admin/categories/show.blade.php
+++ resources/views/admin/categories/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin')
 @section('content')
     <h1>{{ $category->name }}</h1>
+    @if (count($category->projects ) > 0)
     <p>
         <h4>Progetti appartenenti a questa categoria:</h4>
         <ul class="fs-5">
@@ -9,4 +10,7 @@
             @endforeach
         </ul>
     </p>
+    @else
+    <p class="fs-3 mt-4">Non esistono progetti che appartengono a questa categoria!</p>
+    @endif
 @endsection
--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.admin')
 @section('content')
     <h1>{{ $tag->name }}</h1>
+
+    @if (count($tag->projects ) > 0)
     <p>
         <h4>Progetti che utilizzano questa tecnologia:</h4>
         <ul class="fs-5">
@@ -9,4 +11,7 @@
             @endforeach
         </ul>
     </p>
+    @else
+    <p class="fs-3 mt-4">Non esistono progetti che utilizzano questa tecnologia!</p>
+    @endif
 @endsection
